4fb5f97e65be961a43c917a861b69f339fbc28dd
illuz <iilluzen@gmail.com>
2014-03-28 23:17:21 +0800

    fzu 2156

---
+++ ACM_CODE/FZU/14March/2156.cpp
@@ -0,0 +1,83 @@
+/*
+*  Author:      illuz <iilluzen[at]gmail.com>
+*  File:        2156.cpp
+*  Create Date: 2014-03-28 23:10:05
+*  Descripton:   
+*/
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <cctype>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <list>
+#include <vector>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <utility>
+#include <algorithm>
+using namespace std;
+/****************************/
+#define LL(x) ((x)<<1)
+#define RR(x) ((x)>>1)
+#define MP(x,y) make_pair(x,y)
+#define RI(x) scanf("%d",&x)
+#define RII(x,y) scanf("%d%d",&x,&y)
+#define RD(x) scanf("%lf",&x)
+#define RDD(x,y) scanf("%lf%lf",&x,&y)
+#define RS(x) scanf("%s",x)
+#define PI(x) printf("%d",x)
+#define PIN(x) printf("%d\n",x)
+#define PS(x) printf("%s",x)
+#define PN() printf("")
+#define rep(i,n) for(int i=0;i<(n);i++)
+#define repu(i,a,b) for(int i=(a);i<(b);i++)
+#define repf(i,a,b) for(int i=(a);i<=(b);i++)
+#define repd(i,a,b) for(int i=(a);i>=(b);i--)
+#define swap(a,b) {int t=a;a=b;b=t;}
+#define clr(a) memset(a,0,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define FI(i,x) for(typeof((x).begin()) i=(x).begin();i!=(x).end();i++)
+/****************************/
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+/****************************/
+const int INF = 0x7f7f7f7f;
+const double EPS = 1e-8;
+const double PI = acos(-1.0);
+const int dx[] = {0,-1,0,1};
+const int dy[] = {1,0,-1,0};
+const int fx[] = {-1,-1,-1,0,0,1,1,1};
+const int fy[] = {-1,0,1,-1,1,-1,0,1};
+/****** TEMPLATE ENDS *******/
+
+const int N = 10100;
+const int MOD = 1000000007;
+
+ll f[N];
+int n, x, y, a, b;
+int mmin;
+
+int main()
+{
+	while (~scanf("%d%d%d%d%d", &n, &x, &y, &a, &b)) {
+		clr(f);
+		f[0] = 1;
+		rep (i, n) {
+			if (i >= x)
+				f[i] = (f[i - x] + f[i]) % MOD;
+			if (i >= y)
+				f[i] = (f[i - y] + f[i]) % MOD;
+		}
+		cout << f[min(a, b)] * f[abs(a - b)] % MOD * f[n - max(a, b)] % MOD << endl;
+	}
+	return 0;
+}
+
