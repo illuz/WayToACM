927f5319ae415acefaa859c8779a38df1341749f
illuz <iilluzen@gmail.com>
2014-09-02 08:32:10 +0800

    networkflow02

--- ACM_CODE/linearprogramming_networkflow/2/02.cpp
+++ ACM_CODE/linearprogramming_networkflow/2/02.cpp
@@ -1,15 +1,12 @@
 /*
 *  Author:      illuz <iilluzen[at]gmail.com>
 *  Blog:        http://blog.csdn.net/hcbbt
-*  File:        01.cpp
-*  Create Date: 2014-08-25 15:41:53
-*  Descripton:  network flow
+*  File:        02.cpp
+*  Create Date: 2014-08-25 20:59:45
+*  Descripton:  min value close graph, convert to Network flow
 */
 
-#include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 #define repf(i,a,b) for(int i=(a);i<=(b);i++)
 
@@ -30,6 +27,7 @@ int head[N];
 int tot;
 
 int n, m, u, v;
+ll w, sum;
 
 void addedge(int u, int v, ll w) {
 	edge[tot].from = u;
@@ -113,32 +111,60 @@ ll DICNIC(int S, int T) {
 
 void init() {
 	tot = 0;
+	sum = 0;
 	memset(head, -1, sizeof(head));
 	// read
-	scanf("%d%d", &m, &n);
-	repf (i, 1, m) addedge(0, i, 1);
-	repf (i, m + 1, n) addedge(i, n + 1, 1);
-	bool rec[N][N] = {0};
-	while (scanf("%d%d", &u, &v) && u != -1) {
-		if (rec[u][v] == 0) {
-			addedge(u, v, 1);
-			rec[u][v] = 1;
+	repf (i, 1, n) {
+		scanf("%lld", &w);
+		sum += w;
+		addedge(0, i, w);
+		char ch;
+		while (scanf("%d", &u)) {
+			addedge(i, n + u, INF);
+			if (getchar() != ' ')
+				break;
 		}
+	}
+
+	repf (i, 1, m) {
+		scanf("%lld", &w);
+		addedge(i + n, n + m + 1, w);
 	}
 }
 
-int main() {
-	init();
-	ll ans = DICNIC(0, n + 1);
-	if (ans) {
-		printf("%lld\n", ans);
-		repf (i, 0, tot - 1) {
-			if (edge[i].from != 0 && edge[i].to != n + 1 && edge[i].from < edge[i].to && edge[i].cap == 0) {
-				printf("%d %d\n", edge[i].from, edge[i].to);
-			}
+void solve() {
+	ll ans = sum - DICNIC(0, n + m + 1);
+	vector<int> ex, eq;
+	repf (i, 1, m)
+		if (dep[i + n] != -1)
+			eq.push_back(i);
+	repf (i, 1, n)
+		if (dep[i] != -1)
+			ex.push_back(i);
+	int sz1 = ex.size(), sz2 = eq.size();
+	if (sz1) {
+		printf("%d", ex[0]);
+		repf (i, 1, sz1 - 1) {
+			printf(" %d", ex[i]);
 		}
-	} else {
-		printf("No Solution!");
+	}
+	puts("");
+	if (sz2) {
+		printf("%d", eq[0]);
+		repf (i, 1, sz2 - 1) {
+			printf(" %d", eq[i]);
+		}
+	}
+	puts("");
+	printf("%lld\n", ans);
+}
+
+int main() {
+	freopen("shuttle.in","r",stdin);
+	freopen("shuttle.out","w",stdout);
+	while (~scanf("%d%d", &n, &m)) {
+		init();
+		solve();
 	}
 	return 0;
 }
